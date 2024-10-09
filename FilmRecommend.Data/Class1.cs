7b2e080b5d5350d0a27b8d49135b65214316b5a1
Nuri Işık <nurii6345@gmail.com>
2024-10-09 14:24:03 +0300

    #FilmRecommend.Data katmanını düzenledik amacına uygun yaptık

--- FilmRecommend.Data/Class1.cs
+++ FilmRecommend.Data/Class1.cs
@@ -44,65 +44,14 @@ namespace FilmRecommend.Data
     }
 
 
-    //Veri tabanına ekleme,silme,güncelleme işlemlerini gerçekleştireceğimiz class
-    public class DataOperations
-    {
-        Methods methods = new();
-        
-        FilmMoodDBContext context = new();
-
-
-        //Veritabanına değişikliklerin kayıt edildiği metod
-       
-
-        MovieCategory movieCategory = new()
-        {
-            CategoryName = "Aksiyon",
-            SubCategories = new List<MovieSubCategory>()
-    {
-        new MovieSubCategory()
-        {
-            SubCategoryName = "Casusuluk",
-
-            Movies = new HashSet<Movie>()
-            {
-                new Movie(){MovieName="Skyfall", ReleaseYear=2012, Director="Sam Mendes", LeadingActor="Daniel Craig", Rating=7.8},
-                new Movie(){MovieName="Mission: Impossible - Fallout", ReleaseYear=2018, Director="Christopher McQuarrie", LeadingActor="Tom Cruise", Rating=7.7},
-                new Movie(){MovieName="Tinker Tailor Soldier Spy", ReleaseYear=2011, Director="Tomas Alfredson", LeadingActor="Gary Oldman", Rating=7.1},
-                new Movie(){MovieName="The Bourne Identity", ReleaseYear=2002, Director="Doug Liman", LeadingActor="Matt Damon", Rating=7.9},
-                new Movie(){MovieName="Bridge of Spies", ReleaseYear=2015, Director="Steven Spielberg", LeadingActor="Tom Hanks", Rating=7.6}
-            }
-        }
-    }
-        };
-      
-        
-        
-        
-        
-
-       
-    }
+   
    
 
 
 
 
 
-    public class Methods
-    {
-        //Veritabanına değişikliklerin kayıt edildiği metod
-        public void AddMoviesToDatabase(MovieCategory movieCategory)
-        {
-            FilmMoodDBContext context = new();
-            // context nesnesi ile MovieCategory'yi veritabanına ekliyoruz
-            context.MovieCategories.Add(movieCategory);
-
-            // Değişiklikleri veritabanına kaydediyoruz
-            context.SaveChangesAsync();
-        }
-    }
-
+    
 
 }
 
--- FilmÖneriDB_/obj/Debug/net8.0/FilmÖneriDB_.AssemblyInfo.cs
+++ FilmÖneriDB_/obj/Debug/net8.0/FilmÖneriDB_.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FilmÖneriDB_")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+735b83517060a759e5b725f92077a8f0f3684c86")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80cef6b9954cac92e7037e295552b344f7573907")]
 [assembly: System.Reflection.AssemblyProductAttribute("FilmÖneriDB_")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FilmÖneriDB_")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
